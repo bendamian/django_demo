486332f712a898bdb4f5da86fef26f04363a6fec
bendamian <badamian@gmail.com>
2024-11-04 14:12:03 +0000

    add geo Django

---
+++ backend/admin.py
@@ -0,0 +1,34 @@
+
+from django.contrib.gis import admin
+from .models import Category, Marker, Area, Polyline
+
+
+admin.site.register(Category)
+
+
+class CustomGeoAdmin(admin.GISModelAdmin):
+    gis_widget_kwargs = {
+        'attrs': {
+            'default_zoom': 4,
+            'default_longitude': 51.509865,
+            'default_latitude': -0.118092
+        }
+
+    }
+
+
+@admin.register(Marker)
+class MakerAdmin(CustomGeoAdmin):
+    list_display = ("name", "street", "city")
+
+
+@admin.register(Area)
+class AriaAdmin(CustomGeoAdmin):
+    list_display = ('name', 'boundary')
+    search_fields = ('name',)
+
+
+@admin.register(Polyline)
+class PolylineAdmin(CustomGeoAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
---
+++ backend/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.contrib.gis.db import models
+from django.contrib.gis.geos import LineString
+
+
+# Create your models here.
+# test model
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Marker(models.Model):
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    name = models.CharField('location name', max_length=225, help_text=" name of the Location")
+    point_geom = models.PointField()
+    image = models.ImageField(upload_to='location_images/', blank=True, null=True)
+    street = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=255, blank=True, null=True)
+    county = models.CharField(max_length=255, blank=True, null=True)
+    postcode = models.CharField(max_length=10, blank=True, null=True)
+    active = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = 'Markers'
+
+    def __str__(self):
+        return f" {self.name},{self.city},{self.street},  {self.county} {self.postcode}"
+
+
+class Area(models.Model):
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    name = models.CharField('polygon name', max_length=225, help_text=" name of the polygon to")
+    boundary = models.PolygonField()
+
+    class Meta:
+        verbose_name_plural = 'Areas'
+
+    def __str__(self):
+        return self.name
+
+
+class Polyline(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    path = models.LineStringField()
+
+    def __str__(self):
+        return self.name
+
+# Create your models here.
